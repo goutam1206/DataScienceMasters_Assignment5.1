8251faeff1793c4c6f97b9d2398c0fa43ba6b8a6
Goutama Sarma Mylavarapu <sarma.goutama@gmail.com>
2018-03-04 10:12:59 +0530

    Added function to handle try except
    The attachment contains the functions to handle exceptions

---
+++ Assignment_5.1.py
@@ -0,0 +1,23 @@
+# The below function has the exception handling
+def divide(numerator,denominator):
+    try:
+        result = numerator / denominator
+        return result
+    except ZeroDivisionError:
+        print("Divide by Zero Exception")
+        return "Divide by Zero Exception"
+        
+
+        
+# Below code calls the function which handles the exception of Zero Division
+while True:
+    try:
+        numerator=int(input("Enter the numerator value: "))
+        denominator=int(input("Enter the denominator value: "))
+        break;
+    except ValueError:
+        print ("Please enter a valid integer value")
+
+result = divide(numerator,denominator)
+print ("Result: ",result)
+    
